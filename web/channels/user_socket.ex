1e72c29930c1cdf96c6e71d4bb5660caf1bc30d6
Vincent Grato <vgrato@gmail.com>
2017-03-23 16:08:18 -0700

    play with websockets

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Talavera.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Talavera.RoomChannel
+  channel "room:*", Talavera.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
